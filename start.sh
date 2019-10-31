a2779b994afa1df48191c2f6c952e13649c3ff18
Reddit Politics Moderators <rpoliticsmods@gmail.com>
2019-10-31 03:36:47 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 /usr/bin/tmux new-session -d -s pmb
-/usr/bin/tmux send-keys -t pmb "cd /home/rpolitics/PoliticsModeratorBot" C-m
+/usr/bin/tmux send-keys -t pmb "~/PoliticsModeratorBot" C-m
 /usr/bin/tmux send-keys -t pmb "pyenv activate pmb" C-m
 /usr/bin/tmux send-keys -t pmb "python start.py" C-m
